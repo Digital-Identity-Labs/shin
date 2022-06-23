183d2ee6115c160372a7034b6fd08b0dc59f4700
Pete Birkinshaw <pete@binary-ape.org>
2022-06-23 17:01:35 +0100

    Top level Shin module now has a few friendly functions

--- lib/shin.ex
+++ lib/shin.ex
@@ -3,20 +3,55 @@ defmodule Shin do
 
   alias Shin.IdP
   alias Shin.HTTP
+  alias Shin.Metrics
+  alias Shin.Reports
 
   def idp(idp, opts) do
     IdP.configure(idp, opts)
   end
 
-  def ping?(idp) do
-    client = Shin.HTTP.client(idp)
+  def reload_service(idp, service) do
+    with {:ok, idp} <- prep_idp(idp),
+         {:ok, service} <- IdP.validate_service(idp, service) do
+      HTTP.get_reload(idp, service)
+    else
+      err -> err
+    end
   end
 
   def metrics(idp) do
-#    idp
-#    |> HTTP.client()
-#    |> HTTP.metrics()
+    HTTP.get_json(idp, idp.metrics_path)
+  end
 
+  def metrics(idp, group) do
+    with {:ok, idp} <- prep_idp(idp),
+         {:ok, group} <- IdP.validate_metric_group(idp, group),
+         metrics_path <- IdP.metrics_path(idp, group) do
+      HTTP.get_json(idp, metrics_path)
+    else
+      err -> err
+    end
+  end
+
+  def report(idp) do
+    report(idp, :idp_info)
+  end
+
+  def report(idp, reporter) do
+    with {:ok, idp} <- prep_idp(idp),
+         {:ok, metrics} <- metrics(idp) do
+      Reports.produce(metrics, reporter)
+    else
+      err -> err
+    end
+  end
+
+  defp prep_idp(idp) when is_binary(idp) do
+    IdP.configure(idp)
+  end
+
+  defp prep_idp(%IdP{base_url: _} = idp) do
+    {:ok, idp}
   end
 
 end
