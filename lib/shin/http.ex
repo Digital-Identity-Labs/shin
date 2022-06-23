0250b3aec43f7cf1e1a899b9bc1c2771d13451b7
Pete Birkinshaw <pete@binary-ape.org>
2022-06-23 17:00:55 +0100

    A much smaller, slightly less inelegant HTTP module

--- lib/shin/http.ex
+++ lib/shin/http.ex
@@ -1,21 +1,29 @@
 defmodule Shin.HTTP do
 
+  alias Shin.IdP
 
-  def client(idp) do
+  def client(idp, type \\ :json) do
 
     middleware = essential_middleware(idp)
+    middleware = case type do
+      :json -> [Tesla.Middleware.JSON | middleware]
+      _ -> middleware
+    end
 
     Tesla.client(middleware)
 
   end
 
-  def metrics(idp) do
-    get_json(idp, idp.metrics_path)
-  end
-
-  defp get_json(idp, path) do
+  def get_json(idp, path) do
     case Tesla.get(client(idp), path) do
       {:ok, result} -> {:ok, result.body}
+      {:error, msg} -> {:error, msg}
+    end
+  end
+
+  def get_reload(idp, service) do
+    case Tesla.get(client(idp, :text), idp.reload_path, query: [id: service]) do
+      {:ok, result} -> {:ok, String.trim(result.body)}
       {:error, msg} -> {:error, msg}
     end
   end
@@ -23,8 +31,14 @@ defmodule Shin.HTTP do
   defp essential_middleware(idp) do
     [
       {Tesla.Middleware.BaseUrl, idp.base_url},
-      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Compression, format: "gzip"},
+      {Tesla.Middleware.Timeout, timeout: idp.timeout}
+
     ]
+  end
+
+  defp json_middleware(idp) do
+    [Tesla.Middleware.JSON]
   end
 
 end
