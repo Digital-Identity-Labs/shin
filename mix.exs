0d5ba9280cbe247df4e47dae7479fbfd90dd9ded
Pete Birkinshaw <pete@binary-ape.org>
2022-06-23 16:59:55 +0100

    Improving mix config for packaging and release as a hex package

--- mix.exs
+++ mix.exs
@@ -6,7 +6,26 @@ defmodule Shin.MixProject do
       app: :shin,
       version: "0.1.0",
       elixir: "~> 1.12",
+      description: "Simple HTTP client for the Shibboleth IdP metrics and reload API",
+      package: package(),
+      name: "Shin",
+      source_url: "https://github.com/Digital-Identity-Labs/shin",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: [
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ],
       deps: deps()
     ]
   end
@@ -14,7 +33,7 @@ defmodule Shin.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
@@ -22,9 +41,26 @@ defmodule Shin.MixProject do
   defp deps do
     [
       {:tesla, "~> 1.4"},
-      {:jason, "~> 1.3"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:jason, "~> 1.3"},
+      {:castore, "~> 0.1.17", only: [:dev, :test], runtime: false},
+      {:apex, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.13.0", only: :test},
+      {:benchee, "~> 1.0.1", only: [:dev, :test]},
+      {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
+      {:earmark, "~> 1.3", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:doctor, "~> 0.17.0", only: :dev, runtime: false}
     ]
   end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Digital-Identity-Labs/shin"
+      }
+    ]
+  end
+
 end
