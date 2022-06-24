b4c9991cab26c1c5a186a4700af4a5bfe1698f2e
Pete Birkinshaw <pete@binary-ape.org>
2022-06-24 22:42:11 +0100

    Update metrics examples, add core group example

--- test/support/metrics_examples.ex
+++ test/support/metrics_examples.ex
@@ -1,8 +1,12 @@
 defmodule MetricsExamples do
 
   def complete do
+    {:ok, file} = File.read("test/support/metrics_complete_1.json")
+    Jason.decode!(file)
+  end
 
-    {:ok, file} = File.read("test/support/complete_1.json")
+  def core do
+    {:ok, file} = File.read("test/support/metrics_core_1.json")
     Jason.decode!(file)
   end
 
