a360f4b69f7e9be4ac0faa1dfacb81ce3d7c66dc
Pete Birkinshaw <pete@binary-ape.org>
2022-06-28 15:45:14 +0100

    Typespecs for Metrics

--- lib/shin/metrics.ex
+++ lib/shin/metrics.ex
@@ -1,14 +1,17 @@
 defmodule Shin.Metrics do
 
+  @spec gauge_ids(data :: map()) :: list()
   def gauge_ids(%{"gauges" => gauges} = data) do
     Map.keys(gauges)
   end
 
+  @spec gauge(data :: map(), gauge :: binary()) :: binary | integer
   def gauge(%{"gauges" => gauges} = data, gauge) do
     Map.get(gauges, gauge, %{})
     |> Map.get("value", nil)
   end
 
+  @spec gauges(data :: map()) :: map()
   def gauges(%{"gauges" => gauges} = data) do
     gauges
     |> Enum.map(
@@ -19,6 +22,7 @@ defmodule Shin.Metrics do
     |> Enum.into(%{})
   end
 
+  @spec map_gauges(data :: map(), keymap :: map()) :: map()
   def map_gauges(%{"gauges" => gauges} = data, keymap) do
     keymap
     |> Enum.map(
@@ -31,14 +35,17 @@ defmodule Shin.Metrics do
     |> Enum.into(%{})
   end
 
+  @spec timer_ids(data :: map()) :: list()
   def timer_ids(%{"timers" => timers} = data) do
     Map.keys(timers)
   end
 
+  @spec timer(data :: map(), timer :: binary()) :: map()
   def timer(%{"timers" => timers} = data, timer) do
     Map.get(timers, timer, %{})
   end
 
+  @spec timers(data :: map()) :: map()
   def timers(%{"timers" => timers} = data) do
     timers
     |> Enum.map(
@@ -49,6 +56,7 @@ defmodule Shin.Metrics do
     |> Enum.into(%{})
   end
 
+  @spec map_timers(data :: map(), keymap :: map()) :: map()
   def map_timers(%{"timers" => timers} = data, keymap) do
     keymap
     |> Enum.map(
