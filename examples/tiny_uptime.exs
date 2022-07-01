47aeda719ac79ed5df8d0296bcd9433c022fef23
Pete Birkinshaw <pete@binary-ape.org>
2022-07-01 21:22:51 +0100

    Use example for example

--- examples/tiny_uptime.exs
+++ examples/tiny_uptime.exs
@@ -1,4 +1,4 @@
 #!/usr/bin/env elixir
 Mix.install([{:shin, "~> 0.1"}])
-{:ok, data} = Shin.report("https://idptest.brunel.ac.uk/idp/", :idp_info)
+{:ok, data} = Shin.report("https://example.com/idp", :idp_info)
 IO.puts(data.uptime)
