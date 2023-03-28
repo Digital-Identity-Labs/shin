51756e1cd580edd73fa8b6c92243cbd661930143
Pete Birkinshaw <pete@binary-ape.org>
2023-03-28 18:07:23 +0100

    Update deps, and add Req

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shin.MixProject do
   def project do
     [
       app: :shin,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       description: "Simple HTTP client for the Shibboleth IdP metrics and reload API",
       package: package(),
@@ -41,12 +41,15 @@ defmodule Shin.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tesla, "~> 1.4"},
-      {:jason, "~> 1.3"},
-      {:castore, "~> 0.1.17", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.5.1"},
+      {:req, "~> 0.3"},
+      {:jason, "~> 1.4"},
+      {:castore, "~> 1.0.1"},
+
+      {:bypass, "~> 2.1", only: :test},
       {:apex, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.5.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.13.0", only: :test},
+      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14 and >= 0.14.4", only: [:dev, :test]},
       {:benchee, "~> 1.0.1", only: [:dev, :test]},
       {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
       {:earmark, "~> 1.3", only: :dev, runtime: false},
