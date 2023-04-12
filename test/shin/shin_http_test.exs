db65ea35840aa6d18119df3182bd7b6ed8c3f9e3
Pete Birkinshaw <pete@binary-ape.org>
2023-04-12 16:51:12 +0100

    Ugh. No fun at all rewriting the HTTP tests, but done

--- test/shin/shin_http_test.exs
+++ test/shin/shin_http_test.exs
@@ -9,52 +9,7 @@ defmodule ShinHTTPTest do
     {:ok, bypass: bypass}
   end
 
-  #  setup_all do
-  #    Tesla.Mock.mock_global(
-  #      fn
-  #        %{
-  #          method: :get,
-  #          url: "https://login.localhost.demo.university/example/some/json"
-  #        } ->
-  #          %Tesla.Env{status: 200, body: MetricsExamples.generic}
-  #
-  #        %{
-  #          method: :get,
-  #          url: "https://login.localhost.demo.university/example/profile/admin/reload-service"
-  #        } ->
-  #          %Tesla.Env{status: 200, body: "Sir Morris, not the finest swordsman in the world but the most enthusiastic!"}
-  #
-  #        %{
-  #          method: :get,
-  #          url: "https://login-miss.localhost.demo.university/idp/profile/admin/reload-service"
-  #        } ->
-  #          %Tesla.Env{status: 404, body: "Error 404\n"}
-  #
-  #        %{
-  #          method: :get,
-  #          url: "https://login-error.localhost.demo.university/idp/profile/admin/reload-service"
-  #        } ->
-  #          %Tesla.Env{status: 500, body: "Error 500\n"}
-  #
-  #        %{
-  #          method: :get,
-  #          url: "https://login-miss.localhost.demo.university/idp/profile/admin/metrics"
-  #        } ->
-  #          %Tesla.Env{status: 404, body: "Error 404\n"}
-  #
-  #        %{
-  #          method: :get,
-  #          url: "https://login-error.localhost.demo.university/idp/profile/admin/metrics"
-  #        } ->
-  #          %Tesla.Env{status: 500, body: "Error 500\n"}
-  #
-  #      end
-  #    )
-  #    :ok
-  #  end
-
   describe "get_data/4" do
-
 
     test "must be passed an IdP record (expected)", %{bypass: bypass} do
 
@@ -128,15 +83,15 @@ defmodule ShinHTTPTest do
         "GET",
         "/example/update.txt",
         fn conn ->
-          Plug.Conn.resp(conn, 200, "The capybara are restless")
+          Plug.Conn.resp(conn, 404, "Error 404")
           |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
         end
       )
 
       {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
       assert {
-               :ok,
-               "The capybara are restless"
+               :error,
+               "Error 404"
              } = HTTP.get_data(idp, "/example/update.txt")
 
     end
@@ -299,14 +254,235 @@ defmodule ShinHTTPTest do
 
   describe "post_data/4" do
 
+    test "must be passed an IdP record (expected)", %{bypass: bypass} do
 
+      Bypass.expect_once(
+        bypass,
+        "POST",
+        "/example/update.json",
+        fn conn ->
+          Plug.Conn.resp(conn, 201, File.read!("test/support/star_destroyer.json"))
+        end
+      )
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+      assert {:ok, _metrics} = HTTP.post_data(idp, "/example/update.json", [data: "data"])
+    end
+
+    test "will raise an error if passed a URL or anything other than an IdP struct" do
+      assert_raise RuntimeError, "Shin.HTTP client requires a Shin.IdP struct as the first parameter!", fn ->
+        HTTP.post_data("https://login.localhost.demo.university/example", "some/json", [data: "data"])
+      end
+    end
+
+    test "returns a map when reading complex JSON data from an available URL", %{bypass: bypass} do
+
+      Bypass.expect_once(
+        bypass,
+        "POST",
+        "/example/update.json",
+        fn conn ->
+          Plug.Conn.resp(conn, 201, File.read!("test/support/star_destroyer.json"))
+          |> Plug.Conn.merge_resp_headers([{"content-type", "application/json"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+      assert {
+               :ok,
+               %{
+                 "starship" => %{
+                   "name" => "Star Destroyer"
+                 }
+               }
+             } = HTTP.post_data(idp, "/example/update.json", [data: "data"])
+    end
+
+    test "returns a string when reading plain text data from an available URL", %{bypass: bypass} do
+
+      Bypass.expect_once(
+        bypass,
+        "POST",
+        "/example/update.txt",
+        fn conn ->
+          Plug.Conn.resp(conn, 201, "The capybara are restless")
+          |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+        end
+      )
+
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+      assert {
+               :ok,
+               "The capybara are restless"
+             } = HTTP.post_data(idp, "/example/update.txt", [data: "data"])
+
+    end
+
+    test "returns an error mentioning 404 if URL is not found", %{bypass: bypass} do
+
+      Bypass.expect_once(
+        bypass,
+        "POST",
+        "/example/update.txt",
+        fn conn ->
+          Plug.Conn.resp(conn, 404, "The capybara are restless")
+          |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+      assert {
+               :error,
+               "Error 404"
+             } = HTTP.post_data(idp, "/example/update.txt", [data: "data"])
+
+    end
+
+    test "returns an error mentioning 500 if server fails", %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "POST",
+        "/example/update.txt",
+        fn conn ->
+          Plug.Conn.resp(conn, 500, "A fake error has occurred")
+          |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+      assert {
+               :error,
+               "Error 500"
+             } = HTTP.post_data(idp, "/example/update.txt", [data: "data"])
+
+    end
+
+    test "sets the user agent to be Shin name plus version", %{bypass: bypass} do
+
+      Bypass.expect_once(
+        bypass,
+        "POST",
+        "/example/update.txt",
+        fn conn ->
+
+          assert ["Shin 0.2.0"] = Plug.Conn.get_req_header(conn, "user-agent")
+
+          Plug.Conn.resp(conn, 201, "We're fine. We're all fine here now, thank you. How are you?")
+          |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port), retries: 0)
+      HTTP.post_data(idp, "/example/update.txt")
+
+    end
+
+    test "By default sends data with a JSON MIME type", %{bypass: bypass} do
+      Bypass.expect_once(
+        bypass,
+        "POST",
+        "/example/update.txt",
+        fn conn ->
+
+          assert ["application/json"] = Plug.Conn.get_req_header(conn, "content-type")
+
+          Plug.Conn.resp(conn, 201, File.read!("test/support/star_destroyer.json"))
+          |> Plug.Conn.merge_resp_headers([{"content-type", "application/json"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(endpoint_url(bypass.port), retries: 0)
+      HTTP.post_data(idp, "/example/update.txt", [data: "data"])
+    end
 
   end
 
-  describe "del_data/4" do
+    describe "del_data/4" do
 
+      test "must be passed an IdP record (expected)", %{bypass: bypass} do
 
-  end
+        Bypass.expect_once(
+          bypass,
+          "DELETE",
+          "/example/update.json",
+          fn conn ->
+            Plug.Conn.resp(conn, 200, File.read!("test/support/star_destroyer.json"))
+          end
+        )
+
+        {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+        assert {:ok, _metrics} = HTTP.del_data(idp, "/example/update.json")
+      end
+
+      test "will raise an error if passed a URL or anything other than an IdP struct" do
+        assert_raise RuntimeError, "Shin.HTTP client requires a Shin.IdP struct as the first parameter!", fn ->
+          HTTP.del_data("https://login.localhost.demo.university/example", "some/json")
+        end
+      end
+
+      test "returns an error mentioning 404 if URL is not found", %{bypass: bypass} do
+
+        Bypass.expect_once(
+          bypass,
+          "DELETE",
+          "/example/update.txt",
+          fn conn ->
+            Plug.Conn.resp(conn, 200, "The capybara are restless")
+            |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+          end
+        )
+
+        {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+        assert {
+                 :ok,
+                 "The capybara are restless"
+               } = HTTP.del_data(idp, "/example/update.txt")
+
+      end
+
+      test "returns an error mentioning 500 if server fails (with retries)", %{bypass: bypass} do
+
+        Bypass.expect(
+          bypass,
+          "DELETE",
+          "/example/update.txt",
+          fn conn ->
+            Plug.Conn.resp(conn, 500, "A fake error has occurred")
+            |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+          end
+        )
+
+        {:ok, idp} = Shin.idp(endpoint_url(bypass.port))
+        assert {
+                 :error,
+                 "Error 500"
+               } = HTTP.del_data(idp, "/example/update.txt")
+
+      end
+
+      test "sets the user agent to be Shin name plus version", %{bypass: bypass} do
+
+        Bypass.expect_once(
+          bypass,
+          "DELETE",
+          "/example/update.txt",
+          fn conn ->
+
+            assert ["Shin 0.2.0"] = Plug.Conn.get_req_header(conn, "user-agent")
+
+            Plug.Conn.resp(conn, 200, "We're fine. We're all fine here now, thank you. How are you?")
+            |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+          end
+        )
+
+        {:ok, idp} = Shin.idp(endpoint_url(bypass.port), retries: 0)
+        HTTP.del_data(idp, "/example/update.txt")
+
+      end
+
+    end
 
   defp endpoint_url(port), do: "http://localhost:#{port}/"
 
