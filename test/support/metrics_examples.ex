5b417f89d12d2f8e39aaafa6e9a12bbf129ed5ba
Pete Birkinshaw <pete@binary-ape.org>
2023-04-13 14:12:27 +0100

    Update metrics examples to have both raw/text versions and parsed/map version of the JSON

--- test/support/metrics_examples.ex
+++ test/support/metrics_examples.ex
@@ -2,19 +2,28 @@ defmodule MetricsExamples do
 
   @moduledoc false
 
-  def generic do
-    {:ok, file} = File.read("test/support/star_destroyer.json")
-    Jason.decode!(file)
+  def generic_raw do
+    File.read!("test/support/star_destroyer.json")
   end
 
-  def complete do
-    {:ok, file} = File.read("test/support/metrics_complete_1.json")
-    Jason.decode!(file)
+  def generic_parsed do
+    Jason.decode!(generic_raw())
   end
 
-  def core do
-    {:ok, file} = File.read("test/support/metrics_core_1.json")
-    Jason.decode!(file)
+  def complete_raw do
+    File.read!("test/support/metrics_complete_1.json")
+  end
+
+  def complete_parsed do
+    Jason.decode!(complete_raw())
+  end
+
+  def core_raw do
+    File.read!("test/support/metrics_core_1.json")
+  end
+
+  def core_parsed do
+    Jason.decode!(core_raw())
   end
 
 end
