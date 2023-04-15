358e45a264abd408b4e23214f52973bfd4ea9173
Pete Birkinshaw <pete@binary-ape.org>
2023-04-15 21:27:19 +0100

    Metadata tests

--- test/shin/shin_metadata_test.exs
+++ test/shin/shin_metadata_test.exs
@@ -180,11 +180,21 @@ defmodule ShinMetadataTest do
 
   describe "providers/1" do
 
-    test "queries IdP metrics to return a list of active metadata providers" do
+    test "queries IdP metrics to return a list of active metadata providers", %{bypass: bypass} do
 
-    end
+      Bypass.expect_once(
+        bypass,
+        "GET",
+        "/idp/profile/admin/metrics",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, MetricsExamples.complete_raw())
+          |> Plug.Conn.merge_resp_headers([{"content-type", "application/json"}])
+        end
+      )
 
-    test "the providers are sorted by name" do
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port), retries: 0)
+
+      assert ["exBilateral", "exLocal", "exOverride", "ukfMDQ"] = Shin.Metadata.providers(idp)
 
     end
 
@@ -192,12 +202,37 @@ defmodule ShinMetadataTest do
 
   describe "reload/3" do
 
-    test "causes the specifed metadata provider to reload, if it exists" do
+    test "causes the specified metadata provider to reload, if it exists", %{bypass: bypass} do
+
+      Bypass.expect_once(
+        bypass,
+        "GET",
+        "/idp/profile/admin/reload-metadata",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, "Metadata reloaded for 'exOverride'\n")
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port), retries: 0)
+      assert {:ok, "Metadata reloaded for 'exOverride'"} =
+               Metadata.reload(idp, "exOverride")
 
     end
 
-    test "returns an error if the metadata provider does not exist" do
+    test "returns an error if the metadata provider does not exist", %{bypass: bypass} do
 
+      Bypass.expect_once(
+        bypass,
+        "GET",
+        "/idp/profile/admin/reload-metadata",
+        fn conn ->
+          Plug.Conn.resp(conn, 404, "Not found\n")
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port), retries: 0)
+      assert {:error, "Metadata reload failed for 'exMissingProvider'"} =
+               Metadata.reload(idp, "exMissingProvider")
     end
 
 
@@ -205,7 +240,70 @@ defmodule ShinMetadataTest do
 
   describe "cache/2" do
 
-    test "causes every entity ID in the provided list to be looked up by the IdP" do
+    test "causes every entity ID in the provided list to be looked up by the IdP", %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/mdquery",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, MetadataExamples.basic_raw())
+          |> Plug.Conn.merge_resp_headers([{"content-type", "application/samlmetadata+xml"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      assert ["https://test.ukfederation.org.uk/entity", "https://indiid.net/idp/shibboleth"] = Metadata.cache(
+               idp,
+               ["https://test.ukfederation.org.uk/entity", "https://indiid.net/idp/shibboleth"]
+             )
+
+    end
+
+    test "only successfully cached entity IDs are returned in the list", %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/mdquery",
+        fn conn ->
+
+          if conn.query_params["entityID"] == "https://test.ukfederation.org.uk/entity" do
+            Plug.Conn.resp(conn, 200, MetadataExamples.basic_raw())
+            |> Plug.Conn.merge_resp_headers([{"content-type", "application/samlmetadata+xml"}])
+          else
+            Plug.Conn.resp(conn, 404, "Error")
+            |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+          end
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      assert ["https://test.ukfederation.org.uk/entity"] = Metadata.cache(
+               idp,
+               ["https://test.ukfederation.org.uk/entity", "https://example.net/unknown"]
+             )
+    end
+
+    test "can also accept a single entity ID as a binary string", %{bypass: bypass} do
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/mdquery",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, MetadataExamples.basic_raw())
+          |> Plug.Conn.merge_resp_headers([{"content-type", "application/samlmetadata+xml"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      assert ["https://test.ukfederation.org.uk/entity"] = Metadata.cache(
+               idp,
+               "https://test.ukfederation.org.uk/entity"
+             )
 
     end
 
@@ -214,7 +312,7 @@ defmodule ShinMetadataTest do
   describe "protocols/0" do
 
     test "lists the available protocols for metadata lookup" do
-
+      assert  [:cas, :saml1, :saml2] = Metadata.protocols()
     end
 
   end
