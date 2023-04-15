5cae4c711a3140edd7ac4ea83d5504d6170ff6f7
Pete Birkinshaw <pete@binary-ape.org>
2023-04-15 15:24:27 +0100

    Assertion present helpful error message if user passes IdP as string

--- lib/shin/assertion.ex
+++ lib/shin/assertion.ex
@@ -5,13 +5,23 @@ defmodule Shin.Assertion do
   alias Shin.HTTP
   alias Shin.Utils
 
-  def query(idp, sp, username, options \\ []) do
+  def query(idp, _sp, _username, options \\ [])
+  def query(idp, _, _, _) when is_binary(idp) do
+    {:error, "IdP record is required"}
+  end
+
+  def query(idp, sp, username, options) do
     query_params = Keyword.merge(Utils.build_attribute_query(idp, sp, username, options), [saml2: true])
     options = Keyword.merge(options, [type: :saml2])
     HTTP.get_data(idp, idp.attributes_path, query_params, options)
   end
 
-  def query!(idp, sp, username, options \\ []) do
+  def query!(idp, sp, username, options \\ [])
+  def query!(idp, _, _, _) when is_binary(idp) do
+    raise "IdP record is required"
+  end
+
+  def query!(idp, sp, username, options) do
     Utils.wrap_results(query(idp, sp, username, options))
   end
 
---
+++ test/support/assertion_examples.ex
@@ -0,0 +1,9 @@
+defmodule AssertionExamples do
+
+  @moduledoc false
+
+  def basic_raw do
+    File.read!("test/support/assertion.xml")
+  end
+
+end
