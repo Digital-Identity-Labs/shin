eaa5fa89a7d7e7c4028b73cfd4ef678dbd11e5b1
Pete Birkinshaw <pete@binary-ape.org>
2023-04-15 15:24:57 +0100

    Assertion tests

--- test/shin/shin_assertion_test.exs
+++ test/shin/shin_assertion_test.exs
@@ -4,12 +4,198 @@ defmodule ShinAssertionTest do
   alias Shin.IdP
   alias Shin.Assertion
 
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
   describe "query/4" do
+
+    test "if passed an IdP, SP's entity ID, and username, it returns appropriate attributes as a SAML assertion/binary string",
+         %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/resolvertest",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, AssertionExamples.basic_raw())
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      assert {:ok, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saml2:Assertion" <> _xml} = Assertion.query(
+               idp,
+               "https://test.ukfederation.org.uk/entity",
+               "pete"
+             )
+
+    end
+
+    test "responds with an error if passed a URL for the IdP", %{bypass: bypass} do
+      url = idp_endpoint_url(bypass.port)
+
+      assert {:error, "IdP record is required"} = Assertion.query(
+               url,
+               "https://test.ukfederation.org.uk/entity",
+               "pete"
+             )
+    end
+
+    test "the SAML assertion is SAML2-compliant", %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/resolvertest",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, AssertionExamples.basic_raw())
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      {:ok, assertion} = Assertion.query(
+        idp,
+        "https://test.ukfederation.org.uk/entity",
+        "pete"
+      )
+
+      assert  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saml2:Assertion" <> _xml = assertion
+      assert  String.contains?(assertion, ~s|Version="2.0"|)
+    end
+
+    test "request needs to be using the resolver test endpoint of the IdP, but with SAML2 option and media type",
+         %{bypass: bypass} do
+
+      ## Otherwise these tests aren't really testing much other than the mock
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/resolvertest",
+        fn conn ->
+
+          assert  "true" = conn.query_params["saml2"]
+          assert ["application/samlassertion+xml"] = Plug.Conn.get_req_header(conn, "accept")
+          assert %{
+                   "principal" => "pete",
+                   "requester" => "https://test.ukfederation.org.uk/entity",
+                   "saml2" => "true"
+                 } = conn.query_params
+
+          Plug.Conn.resp(conn, 200, AssertionExamples.basic_raw())
+          |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port), retries: 0)
+      assert {:ok, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saml2:Assertion" <> _xml} = Assertion.query(
+               idp,
+               "https://test.ukfederation.org.uk/entity",
+               "pete"
+             )
+    end
 
   end
 
   describe "query!/4" do
 
+    test "if passed an IdP, SP's entity ID, and username, it returns appropriate attributes as a SAML assertion/binary string",
+         %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/resolvertest",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, AssertionExamples.basic_raw())
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      assert "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saml2:Assertion" <> _xml = Assertion.query!(
+               idp,
+               "https://test.ukfederation.org.uk/entity",
+               "pete"
+             )
+
+    end
+
+    test "raises an exception if passed a URL for the IdP", %{bypass: bypass} do
+      url = idp_endpoint_url(bypass.port)
+
+      assert_raise RuntimeError, "IdP record is required", fn ->
+        Assertion.query!(
+          url,
+          "https://test.ukfederation.org.uk/entity",
+          "pete"
+        )
+      end
+
+    end
+
+    test "the SAML assertion is SAML2-compliant", %{bypass: bypass} do
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/resolvertest",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, AssertionExamples.basic_raw())
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port))
+
+      assertion = Assertion.query!(
+        idp,
+        "https://test.ukfederation.org.uk/entity",
+        "pete"
+      )
+
+      assert  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saml2:Assertion" <> _xml = assertion
+      assert  String.contains?(assertion, ~s|Version="2.0"|)
+    end
+
+    test "request needs to be using the resolver test endpoint of the IdP, but with SAML2 option and media type",
+         %{bypass: bypass} do
+
+      ## Otherwise these tests aren't really testing much other than the mock
+
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/idp/profile/admin/resolvertest",
+        fn conn ->
+
+          assert  "true" = conn.query_params["saml2"]
+          assert ["application/samlassertion+xml"] = Plug.Conn.get_req_header(conn, "accept")
+          assert %{
+                   "principal" => "pete",
+                   "requester" => "https://test.ukfederation.org.uk/entity",
+                   "saml2" => "true"
+                 } = conn.query_params
+
+          Plug.Conn.resp(conn, 200, AssertionExamples.basic_raw())
+          |> Plug.Conn.merge_resp_headers([{"content-type", "text/plain"}])
+        end
+      )
+
+      {:ok, idp} = Shin.idp(idp_endpoint_url(bypass.port), retries: 0)
+      assert "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saml2:Assertion" <> _xml = Assertion.query!(
+               idp,
+               "https://test.ukfederation.org.uk/entity",
+               "pete"
+             )
+    end
+
   end
+
+
+  defp idp_endpoint_url(port), do: "http://localhost:#{port}/idp"
+
 
 end
--- test/shin/shin_attributes_test.exs
+++ test/shin/shin_attributes_test.exs
@@ -3,8 +3,24 @@ defmodule ShinAttributesTest do
 
   alias Shin.IdP
   alias Shin.Attributes
-  
+
   describe "query/4" do
+
+    test "if passed an IdP, SP's entity ID, and username/principal, it returns appropriate attributes, etc as a map" do
+
+    end
+
+    test "responds with an error if passed a URL for the IdP" do
+
+    end
+
+    test "the results include the target SP's entity ID" do
+
+    end
+
+    test "the results include the username/principal" do
+
+    end
 
   end
 
